42e3585cd5d6a08afac8049bcc91d9e70889a9c3
Muhammad Usama <162746442+MUsama111@users.noreply.github.com>
2024-03-18 21:38:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var PersonName = "Albert Einstein";
+console.log("".concat(PersonName, " once said, \u201CA person who never made a mistake never tried anything new.\u201D"));
+var Quote = "“A person who never made a mistake never tried anything new.”";
+var Auther = "Albert Einstein";
+console.log("".concat(Auther, ", once said, \"").concat(Quote));
---
+++ app.ts
@@ -0,0 +1,5 @@
+let PersonName: String="Albert Einstein";
+console.log(`${PersonName} once said, “A person who never made a mistake never tried anything new.”`);
+let Quote: String= "“A person who never made a mistake never tried anything new.”";
+let Auther: String= "Albert Einstein";
+console.log(`${Auther}, once said, "${Quote}`);
